59591971e5aeb60ff36aeada269e2120a9f2c6af
Kevin Tung <kekekevin@gmail.com>
2022-05-11 18:36:20 -0500

    updated design

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,8 @@
   <!--  button bar  -->
 
   <div class="flex justify-center">
-    <div class="w-72 flex flex-col bg-gray-800 rounded-lg">
-      <div class="flex flex-col items-center rounded-t bg-gray-600 text-white mb-2 py-2">
+    <div class="flex flex-col w-72">
+      <div class="flex flex-col bg-gray-600 text-white items-center py-2 rounded-t-lg">
         <div class="">
           AMTD Digital Inc
         </div>
@@ -19,30 +19,30 @@
         </div>
       </div>
 
-      <div class="flex flex-col items-center mb-4">
+      <div class="flex flex-col items-center bg-gray-800 pb-4 pt-4">
         <div class="text-gray-500 text-sm">
           Price Range
         </div>
-        <div class="text-white text-2xl font-bold">
+        <div class="text-white font-bold text-2xl">
           $10.00 - 10.00
         </div>
       </div>
 
-      <div class="flex flex-row mb-4">
+      <div class="flex flex-row bg-gray-800 pb-4">
         <div class="flex flex-col flex-grow">
-          <div class="text-center text-gray-500 text-sm">Anticipated</div>
-          <div class="text-center text-white text-xs">July 20th</div>
+          <div class="text-gray-500 text-center text-sm">Anticipated</div>
+          <div class="text-white text-center text-sm">July 20th</div>
         </div>
 
         <div class="flex flex-col flex-grow">
-          <div class="text-center text-gray-500 text-sm">Sector</div>
-          <div class="text-center text-white text-xs">SPAC</div>
+          <div class="text-gray-500 text-center text-sm">Sector</div>
+          <div class="text-white text-center text-sm">SPAC</div>
         </div>
       </div>
 
-      <div class="flex flex-row gap-x-2 mb-4 mx-8">
-        <button class="rounded text-white bg-blue-700 flex-grow">Order</button>
-        <button class="rounded text-white bg-transparent flex-grow">Favorite</button>
+      <div class="flex flex-row gap-x-2 bg-gray-800 pb-4 px-8 rounded-b-lg">
+        <button class="rounded flex-grow text-white bg-blue-500">Order</button>
+        <button class="rounded flex-grow text-white">Favorite</button>
       </div>
     </div>
   </div>
