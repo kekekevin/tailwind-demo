74517ab0ac20fa63821b18f827deb13fddf46a1e
Kevin Tung <kekekevin@users.noreply.github.com>
2022-05-10 23:21:36 -0500

    tailwind initialized

---
+++ backup/Card.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let name
+  export let symbol
+</script>
+
+<div class="w-72 flex flex-col bg-gray-800 rounded-lg">
+  <div class="flex flex-col items-center rounded-t bg-gray-600 text-white mb-2 py-2">
+    <div class="">
+      {name}
+    </div>
+    <div class="font-bold text-lg">
+      {symbol}
+    </div>
+  </div>
+
+  <div class="flex flex-col items-center mb-4">
+    <div class="text-gray-500 text-sm">
+      Price Range
+    </div>
+    <div class="text-white text-2xl font-bold">
+      $10.00 - 10.00
+    </div>
+  </div>
+
+  <div class="flex flex-row mb-4">
+    <div class="flex flex-col flex-grow">
+      <div class="text-center text-gray-500 text-sm">Anticipated</div>
+      <div class="text-center text-white text-xs">July 20th</div>
+    </div>
+
+    <div class="flex flex-col flex-grow">
+      <div class="text-center text-gray-500 text-sm">Sector</div>
+      <div class="text-center text-white text-xs">SPAC</div>
+    </div>
+  </div>
+
+  <div class="flex flex-row gap-x-2 mb-4 mx-8">
+    <button class="rounded text-white bg-blue-700 flex-grow">Order</button>
+    <button class="rounded text-white bg-transparent flex-grow">Favorite</button>
+  </div>
+</div>
